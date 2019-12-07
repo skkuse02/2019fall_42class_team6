b474f667490076ee3401774dc10f976f9e43364d
shb0107s <shb0107s@naver.com>
2019-12-07 14:50:11 +0900

    .
    .

--- web/frontend/src/components/Secure.vue
+++ web/frontend/src/components/Secure.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div class="securePage">
     <h1>This page is protected by auth</h1>
+    <br>
+    <b-button class="homeBtn" v-on:click="moveTo" variant="secondary">Go to Home</b-button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    moveTo(){
+      this.$router.push('/').catch(err => {});
+    }
+  }
+}
+</script>
+
+<style media="screen">
+  .securePage{
+    display: inline-block;
+    text-align: center;
+  }
+</style>
