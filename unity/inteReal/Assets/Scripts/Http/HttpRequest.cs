54583cece7211cf029e0a26212f20bee6f012abc
arensis-julia <julia981028@gmail.com>
2019-12-07 18:04:42 +0900

    feat: Add chair 3d model(.obj) feat: HTTP request coroutine feat: Re-location of furnitures fix: Better placement logic

--- unity/inteReal/Assets/Scripts/Http/HttpRequest.cs
+++ unity/inteReal/Assets/Scripts/Http/HttpRequest.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
 using UnityEditor;
 using System.IO;
 
-public class HttpRequest
+public class HttpRequest: MonoBehaviour
 {
-    public string Get(string endpoint, Dictionary<string, string> parameters) {
-        return GetTestJSON();
+    public IEnumerator Get(string endpoint, Dictionary<string, string> parameters) {
+        /* call StartCoroutine(Get()); to where the request is needed */
+        List<string> paramString = new List<string>();
+        foreach (var p in parameters) {
+            paramString.Add(string.Format("{0}={1}", p.Key, p.Value));
+        }
+        endpoint += "?" + string.Join("&", paramString);
+
+        UnityWebRequest uwr = UnityWebRequest.Get(endpoint);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError) {
+            Debug.Log("Error while GET: " + uwr.error);
+        }
+        else {
+            yield return uwr.downloadHandler.text;
+        }
     }
 
     [MenuItem("Tools/Read file")]
